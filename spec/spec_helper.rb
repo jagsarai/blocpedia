64b2b5d73fc85c134653c92553c9f31a5f35855d
jagsarai <sarai.jagvir@gmail.com>
2017-07-04 16:15:53 -0700

    add the edit profile route

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,11 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+
+  # gives us access to sign_in and sign_out in our tests
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
